5a761eae334f4d362dbabc02399368a97abdb01a
xxgameryt123 <63223801+xxgameryt123@users.noreply.github.com>
2020-04-09 10:47:13 +0200

    TBomb2

--- TBomb2.sh
+++ TBomb2.sh
@@ -25,20 +25,20 @@ rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
 figlet TBomb
-echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
-echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;34m Creato da XxGamerYT \e[1;32m"
+toilet -f mono12 -F 
+echo -e "\e[4;34m TBomb Creato Da XxGamerYT \e[0m"
+echo -e "\e[1;34m \e[0m"
+echo -e "\e[1;32m           Mail: XxGamerYT \e[0m"
+echo -e "\e[4;32m   YouTube Page: XxGamerQSDProXx ElectroFazYT \e[0m"
 echo " "
-echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo -e "\e[4;31m Telegram @DIAVOLOGAMER !!! \e[0m"
 echo " "
-echo "Press 1 To  Start SMS Bomber "
-echo "Press 2 To  Start Call Bomber "
-echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
-echo "Press 4 To  View Features "
-echo "Press 5 To  Exit "
+echo "🔥Premi 1 per spammare sms🔥 "
+echo "⚡Premi 2 per spammare chiamate⚡ "
+echo "🌠Premi 3 per aggiornare TBomb🌠 🌀(per Kali Linux e termux)🌀 "
+echo "😘Premi 4 per vedere gli aggiornamenti😘 "
+echo "💥Premi 5 per uscire💥 "
 read ch
 if [ $ch -eq 1 ];then
 clear
